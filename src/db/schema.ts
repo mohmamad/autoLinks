84171f2bc131b8f6925c4d34c0da9df09f01096a
mohmamad <mohamad.isa.moghrabi@gmail.com>
2026-03-08 03:14:10 +0200

    database initialization

---
+++ src/db/schema.ts
@@ -0,0 +1,27 @@
+import { pgTable, serial, text, varchar, integer } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: serial("id").primaryKey(),
+
+  username: varchar("username", { length: 255 }).notNull(),
+
+  email: varchar("email", { length: 255 }).notNull().unique(),
+
+  password: text("password").notNull(),
+});
+
+export const pipelines = pgTable("pipelines", {
+  id: serial("id").primaryKey(),
+
+  name: varchar("name", { length: 255 }).notNull(),
+
+  action: varchar("action", { length: 255 }).notNull(),
+
+  incomingWebhook: text("incoming_webhook").notNull(),
+
+  outgoingWebhook: text("outgoing_webhook").notNull(),
+
+  userId: integer("user_id")
+    .notNull()
+    .references(() => users.id),
+});

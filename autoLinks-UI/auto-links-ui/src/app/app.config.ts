7f449a6e843e11f6ebd83671ad4a0dd701c78226
mohmamad <mohamad.isa.moghrabi@gmail.com>
2026-03-24 09:41:26 +0200

    fix vercel build

--- autoLinks-UI/auto-links-ui/src/app/app.config.ts
+++ autoLinks-UI/auto-links-ui/src/app/app.config.ts
@@ -1,16 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import {
-  provideHttpClient,
-  withInterceptors,
-} from '@angular/common/http';
+import { provideRouter, withHashLocation } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     //make HttpClient available for injection everywhere in the app.
     //withInterceptors([authInterceptor]) - interceptors are applied to all HTTP requests.
     provideHttpClient(withInterceptors([authInterceptor])),
